14237efd7beba9529f2d93484f27d73953ee90eb
Bartosz Szymanski <kontakt@bartoszszymanski.net>
2017-10-26 12:43:19 +0200

    Path builder added

--- server/services/databaseService.js
+++ server/services/databaseService.js
@@ -1,7 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path')
+const dbPath = path.resolve(__dirname, 'todo.db')
+const db = new sqlite3.Database(dbPath)
 
 function initializeConnectionWithDataBase() {
-  return new sqlite3.Database('./db/brightPixel.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,(err) => {
+  return new sqlite3.Database('./db/brightPixel.db', (sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE),(err) => {
     if (err) {
       console.error(err.message);
     }
