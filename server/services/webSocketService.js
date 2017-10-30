4e20d88482f7c9511a3072f689182ba90f0ea5ad
Marsel Atnyashev <atn@marsel.name>
2017-10-30 21:48:11 +0500

    Fix connection using ws

--- server/services/webSocketService.js
+++ server/services/webSocketService.js
@@ -1,66 +1,23 @@
-const SockJS = require('sockjs-client');
 const configurationService = require('./configurationService');
 const encryptionService = require('./encryptionService');
 const registrationService = require('./registrationService');
 const WebSocket = require('ws');
-const io = require('socket.io-client');
 
 function establishConnectionWithWebSocket() {
   return configurationService.getUserAndPasswordFromConfiguration()
     .then((config) => {
-      const url = `ws://${registrationService.API_URL}${'/player/'}${config.playerId}${'/ws'}`;
-      // var missing part = ${encryptionService.encode(config.user)}:${encryptionService.encode(config.password)}$;
-    //   const sock = new SockJS(url);
-    //
-    //   sock.onopen = function() {
-    //     console.log('open');
-    //   };
-    //   sock.onmessage = function(e) {
-    //     console.log('message', e.data);
-    //   };
-    //   sock.onclose = function() {
-    //     console.log('close');
-    //   };
-    //
-    //   setInterval(function() {
-    //     console.log('...');
-    //   }, 10000);
-    // })
-    // .catch((error) => {
-    //   console.error(error);
-    // });
-      const userpass = `${config.user}:${config.password}`;
-      const socket = io.connect(url, {
-        transportOptions: {
-          polling: {
-            extraHeaders: {
-              Authorization: `Basic ${encryptionService.encode(userpass)}`
-            }
-          }
+      const url = `ws://${registrationService.API_URL}/api/1/player/${config.playerId}/ws`;
+      const userpass = `${config.playerId}:${config.password}`;
+      const ws = new WebSocket(url, {
+        perMessageDeflate: false,
+        headers: {
+          Authorization: `Basic ${encryptionService.encode(userpass)}=`
         }
       });
-
-// Add a connect listener
-      socket.on('connect', function(socket) {
-        console.log('Connected!');
+      
+      ws.on('open', function open() {
+        console.log('connected');
       });
-      socket.on('error', function(error) {
-        console.log(error);
-      });
-      socket.on('close', function() {
-        console.log('closed connection');
-      });
-      socket.on('event', function(data) {
-        console.log(data);
-      });
-
-      setInterval(function() {
-        console.log('...');
-      }, 10000);
-
-      socket.emit('CH01', 'me', 'test msg');
-      console.log('Message sent');
-      socket.close();
     });
 }
 
