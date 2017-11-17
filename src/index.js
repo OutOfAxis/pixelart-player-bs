f832563087a3bed22a8095d8a5428da79549ee22
Bartosz Szymanski <kontakt@bartoszszymanski.net>
2017-11-17 13:48:11 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ const registrationService = require('./server/services/registrationService');
 const webSocket = require('./server/infrastructure/webSocket');
 const fileHandler = require('./server/utils/fileHandler');
 const config = require('./server/utils/config');
-const logger = require('./server/utils/logger');
-logger.logInfo.info('BAGNO');
-logger.logError.info('BAGNO');
 
 (async function() {
   await fileHandler.initDirectories(config.CONTENT_ADDRESS);
