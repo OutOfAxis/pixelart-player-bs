51987278fda9dc3fe87629885124007d37bea478
Bartosz Szymanski <kontakt@bartoszszymanski.net>
2017-11-30 16:23:24 +0100

    Restore config

--- src/server/utils/config.js
+++ src/server/utils/config.js
@@ -1,15 +1,16 @@
 const BASE_URL = 'b.pixelart.ge:5300';
 const REST_API_URL = `http://${BASE_URL}/api/1/player/register/`;
 const WS_API_URL = `ws://${BASE_URL}/api/1/player/`;
-const LOCAL = '/storage/sd/'; // '/Users/bartosz/Repositories/pixelart-player-bs';
+const LOCAL = '/Users/bartosz/Repositories/pixelart-player-bs';
 const DATABASE_ADDRESS = `${LOCAL}`;
-const TMP_DIRECTORY = '';// '/tmp/';
+const TMP_DIRECTORY = '/storage/sd/';
 const CONTENT_ADDRESS = `${LOCAL}/content/`;
 const PLAYLIST_ADDRESS = `${CONTENT_ADDRESS}/playlist.json`;
 
 module.exports = {
   REST_API_URL,
   WS_API_URL,
+  LOCAL,
   DATABASE_ADDRESS,
   PLAYLIST_ADDRESS,
   CONTENT_ADDRESS,
