39f5d9f1ba89ebfb805bee42fa1e6a0e5d99ad6b
Ilya Igonkin <osdev0@gmail.com>
2018-05-02 15:54:59 +0400

    New build: w/o ping sending

--- src/server/infrastructure/webSocket.js
+++ src/server/infrastructure/webSocket.js
@@ -20,18 +20,18 @@ async function establishConnectionWithWebSocket() {
   ws.on('open', function open() {
     console.log('Connection to WebSocket has been established.');
 
-    pingerIntervalId = setInterval(function timeout() {
-      ws.ping(function(error) {
-        if (error) {
-          console.error(`Sending ping failed: ${ error }`);
-          ws.terminate();
-        }
-      });
-    }, 5000);
+    // pingerIntervalId = setInterval(function timeout() {
+    //   ws.ping(function(error) {
+    //     if (error) {
+    //       console.error(`Sending ping failed: ${ error }`);
+    //       ws.terminate();
+    //     }
+    //   });
+    // }, 5000);
   });
 
   ws.on('connection', function open() {
-    console.log('connectged');
+    console.log('connected');
   });
 
   ws.on('message', function incoming(data) {
@@ -41,11 +41,11 @@ async function establishConnectionWithWebSocket() {
   });
 
   ws.on('ping', function ping() {
-    // console.log('Ping received');
+    console.log('Ping received');
   });
 
   ws.on('pong', function pong() {
-    // console.log('Pong received');
+    console.log('Pong received');
   });
 
   ws.on('error', function error(err) {
